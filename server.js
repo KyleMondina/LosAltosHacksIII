3b81d3f83a7803ea4196bbf83cf6034903fc4df5
KyleMondina <kylemondina@gmail.com>
2018-03-25 04:23:59 -0700

    almost done

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const routes = require('./routes.js');
 const socket = require('socket.io');
 const path = require('path');
 
-const server = app.listen(process.env.PORT);
+const server = app.listen(4000);
 const io = socket(server);
 
 app.use(express.static(path.join(__dirname, 'Public')));
@@ -14,7 +14,7 @@ app.use(routes);
 
 io.on('connection', socket => {
     console.log('made socket connection');
-    
+
     socket.on('gameCreated', () => io.sockets.emit('enableGame'));
     socket.on('problemSubmited', data => {
         console.log(data);
@@ -23,25 +23,25 @@ io.on('connection', socket => {
     socket.on('answerSubmited', data => {
         console.log(data.studentName);
         io.sockets.emit('answerSent', data);
-        
+
     });
     socket.on('pauseGame', () => io.sockets.emit('gamePaused'));
-    socket.on('resumeGame', () => io.sockets.emit('gameResumed'));
-    
+    socket.on('resumeGame', data => io.sockets.emit('gameResumed', data));
+
     socket.on('endGame', data => {
+      console.log('endGAme');
         console.log(data);
         io.sockets.emit('gameEnded', data)
-        
-    });
-    
-    socket.on('passWinnerData', data => {
-        console.log(data);
-        var array = {};
-        array.push(data)
-        
-        io.sockets.emit('dataToLB', array)
-        
-    });
-    
-});
 
+    });
+
+    socket.on('passWinnerData', data => {
+      console.log('passWinnerDAta');
+        console.log(data);
+
+
+        io.sockets.emit('dataToLB', data);
+
+    });
+
+});
